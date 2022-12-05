46497d035b18671b43af7f1b12e69fdf96414baa
Daniel Petrov <104331624+petrovpix@users.noreply.github.com>
2022-12-05 18:13:02 +0100

    Added Solution for part1.6 Step 5

---
+++ Solutions/part16_step5.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+link = "http://suninjuly.github.io/find_link_text"
+
+try:
+    browser = webdriver.Firefox()
+    browser.get(link)
+
+    link = browser.find_element(By.LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
+    link.click()
+
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # You will have 30sec to copy text from browser window
+    time.sleep(30)
+    # browser close
+    browser.quit()
+
+# leave empty line in the end of file
